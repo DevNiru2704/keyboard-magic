fc935e049a116633863dad6ecf47399041acd9b7
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-07-25 11:36:33 +0000

    Added support for touchscreen devices

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const body=document.querySelector("body")
-const displayBox=document.querySelector(".display-box")
-const keyboard=document.querySelector(".display-box p")
-body.addEventListener("keydown",function(e){
-    const keys=e.key
-    if(keys===" "){
-        keyboard.innerHTML="Space"
-    }else{
-        keyboard.innerHTML=`${keys}`
-    }
-    console.log(keys)
+const body = document.querySelector("body");
+const displayBox = document.querySelector(".display-box");
+const inputBox = document.querySelector("#input-box");
+const keyboard = document.querySelector(".display-box p");
+
+inputBox.value = "";
+
+
+body.addEventListener("keydown", function(e) {
+    const key = e.key === " " ? "Space" : e.key;
+    keyboard.innerHTML = key === "Unidentified" || key === "" ? "Backspace" : key;
+    console.log(key);
+});
+body.addEventListener("touchstart", function() {
+    inputBox.focus();
 })
+inputBox.addEventListener("input", function() {
+    const inputText = inputBox.value.trim()
+    keyboard.innerHTML = inputText === "" ? "Space" : inputText
+    inputBox.value = ""
+})
+
+function is_touch_enabled() {
+    return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+}
+
+if(is_touch_enabled){
+    document.querySelector(".desktop-warning").style.display="none"
+}else{
+    document.querySelector(".mobile-warning").style.display="none"
+}
